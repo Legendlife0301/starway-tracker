bf50bed6c1a9543f6306e2b4d6f68fc00614a348
Legendlife0301 <legendlife0301@gmail.com>
2024-09-21 13:09:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -205,3 +205,17 @@ client.on('guildMemberRemove', (member) => {
         }
     }
 })
+
+// Sunucu oluşturma ve proje aktivitesi sağlama.
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Web sunucu
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} numaralı bağlantı noktasında yürütülüyor.`);
+});
